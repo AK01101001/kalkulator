684d57104edeab74c2811100c05cd2c6c4a1d241
AK01101001 <147595111+AK01101001@users.noreply.github.com>
2024-11-20 16:11:59 +0100

    Add files via upload

---
+++ kalkulator/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kalkulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int numb1,numb2,value;
+            if(int.TryParse(num1.Text, out numb1)&& int.TryParse(num2.Text, out numb2))
+            {
+                value = numb1 + numb2;
+                MessageBox.Show(value.ToString(),"wynik" ,MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            MessageBox.Show("tak","niepoprawne dane", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
